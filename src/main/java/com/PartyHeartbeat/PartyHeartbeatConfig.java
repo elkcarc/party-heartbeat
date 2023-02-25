4a3dcc543102250ad090d4cc318cbbb2f9434317
elkcarc <peytonhazama@gmail.com>
2023-02-24 20:38:16 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/com/PartyHeartbeat/PartyHeartbeatPlugin.java

--- src/main/java/com/PartyHeartbeat/PartyHeartbeatConfig.java
+++ src/main/java/com/PartyHeartbeat/PartyHeartbeatConfig.java
@@ -16,23 +16,44 @@ public interface PartyHeartbeatConfig extends Config {
         return true;
     }
 
+
     @ConfigItem(
             position = 1,
-            keyName = "ShowTicks",
-            name = "Show timer in ticks",
-            description = "Shows timer in ticks"
+            keyName = "shouldNotify",
+            name = "Notify on Disconnect",
+            description = "Sends a notification event to Runelite on lack of heartbeat from party member (requires Runelite notifications on)"
     )
-    default boolean showTicks() {
+    default boolean shouldNotify() {
         return true;
     }
 
     @ConfigItem(
             position = 2,
+            keyName = "shouldNotifySound",
+            name = "Notification Sound on Disconnect",
+            description = "Sends a notification sound on party member DC"
+    )
+    default boolean shouldNotifySound() {
+        return false;
+    }
+
+    @ConfigItem(
+            position = 3,
+            keyName = "shouldNotifyFlash",
+            name = "Notification Flash on Disconnect",
+            description = "Sends a custom notification flash on party member DC"
+    )
+    default boolean shouldNotifyFlash() {
+        return false;
+    }
+
+    @ConfigItem(
+            position = 4,
             keyName = "maxTicks",
             name = "Maximum ticks",
             description = "Maximum ticks without pulse before overlay render"
     )
     default int maxTicks() {
-        return 2;
+        return 3;
     }
 }
